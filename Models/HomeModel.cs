df7cd49dc0d55ed7c461e7aff61135831b418d15
Krisha Busa <23010101039@darshan.ac.in>
2025-07-09 22:02:50 +0530

    Add files via upload

---
+++ Models/HomeModel.cs
@@ -0,0 +1,17 @@
+﻿namespace QuizManagement.Models
+{
+    public class HomeModel
+    {
+        public int Easy {  get; set; }
+
+        public int Medium { get; set; } 
+
+        public int Hard {  get; set; }
+
+        public int TotalQuizes {  get; set; }
+
+        public int TotalQuestions { get; set; }
+
+        public int TotalQuestionLevels { get; set; }
+    }
+}
